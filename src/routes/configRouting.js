4bd02770a8e27e8bc9028f67aa1435657ef79865
martinmolina1988 <martincarp35@gmial.com>
2020-12-25 09:21:23 -0300

    sexto

--- src/routes/configRouting.js
+++ src/routes/configRouting.js
@@ -1,9 +1,5 @@
 import Home from "../page/Home";
-import Error404 from "../page/error404";
-import Upload from "../components/upload";
-import Producto from "../components/Producto/Producto";
-import CrearProducto from "../components/CrearProducto/CrearProducto";
-import ListProducts from "../components/ListProducts";
+
 
 
 export default [
